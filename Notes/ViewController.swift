8c7e1854ef7dd3f1f77ce70cf4adfa2e1449597d
Nikolai Astakhov <skynetprojectone@gmail.com>
2023-01-23 14:19:56 +0300

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Nikolai Astakhov on 23.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
